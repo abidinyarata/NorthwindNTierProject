89d683e9534a0da21b15970c3222937d67353627
abidinyarata <abidinyarata@windowslive.com>
2021-12-30 14:51:55 +0300

    Category için ViewComponentModel sınıfı oluşturuldu.

---
+++ Northwind.Model/ComponentVM/CategoryVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Northwind.Model.ComponentVM
+{
+    public class CategoryVM
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
