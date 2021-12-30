6075282321d1957b5966f0c4bc2234398d1fe144
abidinyarata <abidinyarata@windowslive.com>
2021-12-30 14:50:26 +0300

    Category için service ve repository sınıfları yazıldı

---
+++ Northwind.Business/CategoryService.cs
@@ -0,0 +1,32 @@
+﻿using Northwind.DataAccess;
+using Northwind.Entities;
+using Northwind.Model.ComponentVM;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Northwind.Business
+{
+    public class CategoryService
+    {
+        CategoryRepository _categoryRepository = new CategoryRepository();
+
+        public List<CategoryVM> GetCategories()
+        {
+            List<Category> categories = _categoryRepository.GetCategories();
+            List<CategoryVM> viewCategories = new List<CategoryVM>();
+
+            CategoryVM categoryVM;
+            foreach (Category category in categories)
+            {
+                categoryVM = new CategoryVM
+                {
+                    CategoryID = category.CategoryId,
+                    CategoryName = category.CategoryName
+                };
+                viewCategories.Add(categoryVM);
+            }
+
+            return viewCategories.OrderBy(c => c.CategoryName).ToList();
+        }
+    }
+}
---
+++ Northwind.DataAccess/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Northwind.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Northwind.DataAccess
+{
+    public class CategoryRepository
+    {
+        NorthwindDBContext _dbContext = new NorthwindDBContext();
+
+        public List<Category> GetCategories()
+        {
+            return _dbContext.Categories.ToList();
+        }
+    }
+}
