87dc8c3baf3f2eb8a560d0c2622dc732119880ca
abidinyarata <abidinyarata@windowslive.com>
2021-12-30 14:17:21 +0300

    Proje katmanları oluşturulup, DB First yaklaşımı ile Northwind database eklendi. Client Side kütüphaleri eklendi. (Bootstrap Css, Js, Jquery)

---
+++ Northwind.Entities/Gonderim.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Northwind.Entities
+{
+    public partial class Gonderim
+    {
+        public string CompanyName { get; set; }
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+    }
+}
---
+++ Northwind.Entities/NewCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Northwind.Entities
+{
+    public partial class NewCategory
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+    }
+}
